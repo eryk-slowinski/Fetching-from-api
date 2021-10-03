48bf05b52733c4b378ef45467d154dc9647fb30f
eryk-slowinski <e.slowinski92@gmail.com>
2021-10-03 09:14:42 +0200

    Added components to main html and imported them into app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { JobListComponent } from './components/jobList/jobList.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, HeaderComponent, JobListComponent],
+  imports: [BrowserModule, HttpClientModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
